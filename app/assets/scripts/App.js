d751adbbe6ac7140fd273ae3ee507e6198197c72
Alan Tran <contactalantran@gmail.com>
2020-10-21 09:06:16 -0400

    finish lazyloading

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,15 +2,37 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import DesktopMenu from './modules/DesktopMenu'
+import 'lazysizes'
 
-let mobileMenu = new MobileMenu();
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 0, .9);
-new RevealOnScroll(document.querySelectorAll(".headline"), 0, .9);
-new RevealOnScroll(document.querySelectorAll(".review"), 0, .9);
+new MobileMenu();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 0, .6);
+new RevealOnScroll(document.querySelectorAll(".headline"), 0, .6);
+new RevealOnScroll(document.querySelectorAll(".review"), 0, .6);
 
 new DesktopMenu("my-beginning", 5, .3) //parameters: section, rootMargin, threshold
 new DesktopMenu("features", -15, .3)
 new DesktopMenu("reviews", -15, .4)
+
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(button => {
+    button.addEventListener("click", e => {
+        e.preventDefault();
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */'./modules/Modal')
+            .then(importedFile => {
+                modal = new importedFile.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            })
+            .catch(() => {
+                console.log("error!")
+            })
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
+
 
 if (module.hot) {
     module.hot.accept()
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,51 @@
+class Modal {
+    constructor() {
+        this.injectHTML()
+        this.modal = document.querySelector(".modal")
+        this.closeIcon = document.querySelector(".modal__close")
+        this.events()
+    }
+
+    events() {
+        // listen for close click
+        this.closeIcon.addEventListener("click", () => this.closeTheModal())
+        // listen for keys
+        document.addEventListener("keyup", e => this.keyPressHandler(e))
+    }
+
+    keyPressHandler(e) {
+        if (e.keyCode == 27) {
+            this.closeTheModal();
+        }
+    }
+
+    openTheModal() {
+        this.modal.classList.add("modal--is-visible")
+    }
+
+    closeTheModal() {
+        this.modal.classList.remove("modal--is-visible")
+    }
+
+    injectHTML() {
+        document.body.insertAdjacentHTML("beforeend", `
+        <div class="modal">
+        <div class="modal__inner">
+          <h2 class="section-title section-title--brown section-title--no-b-padding">Get in <strong>Touch</strong></h2>
+          <div class="wrapper">
+            <p class="modal__description">Online order system coming soon!</p>
+            <p class="modal__description">Until then, check out my instagram or shoot me an email!</p>
+          </div>
+    
+          <div class="social-icons">
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/mail.svg" alt="Gmail""></a>
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
+          </div>
+        </div>
+        <div class="modal__close">X</div>
+      </div>
+        `)
+    }
+}
+
+export default Modal
