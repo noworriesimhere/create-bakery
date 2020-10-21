22c50a780dacc8e8a0c94a928c723dd2786ba0cc
Alan Tran <contactalantran@gmail.com>
2020-10-21 13:53:23 -0400

    complete build process

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -33,13 +33,13 @@ class Modal {
         <div class="modal__inner">
           <h2 class="section-title section-title--brown section-title--no-b-padding">Get in <strong>Touch</strong></h2>
           <div class="wrapper">
-            <p class="modal__description">Online order system coming soon!</p>
-            <p class="modal__description">Until then, check out my instagram or shoot me an email!</p>
+            <p class="modal__description">Online order system coming soon</p>
+            <p class="modal__description">Until then, check out my instagram or shoot me an email :)</p>
           </div>
     
           <div class="social-icons">
-            <a href="#" class="social-icons__icon"><img src="assets/images/icons/mail.svg" alt="Gmail""></a>
-            <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
+            <a href="mailto:gailolivis@gmail.com?subject=Cake Idea Inquiry" class="social-icons__icon"><img src="assets/images/icons/mail.svg" alt="Gmail""></a>
+            <a href="https://www.instagram.com/createbakery/" target=”_blank” class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
           </div>
         </div>
         <div class="modal__close">X</div>
